8f6b1de4f19bc990813bb6844c1374fd081932fc
Carlos Baptiste <c.a.baptiste@gmail.com>
2022-11-15 21:06:52 -0500

    Add project files.

---
+++ DotnetBuildDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using System.Reflection;
+
+Console.WriteLine("Hello from dotnet 6!");
+
+var versionInfo = GetVersionInfo();
+Console.WriteLine($"assemblyVersion = {versionInfo.Item1} ");
+Console.WriteLine($"fileVersion = {versionInfo.Item2} ");
+Console.WriteLine($"productVersion = {versionInfo.Item3} ");
+
+Console.ReadKey();
+
+
+static Tuple<string?, string?, string?> GetVersionInfo()
+{
+    var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
+
+    var fileVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion;
+    var productVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+
+    return new Tuple<string?, string?, string?>(assemblyVersion, fileVersion, productVersion);
+}
---
+++ MSBuildDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSBuildDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from .NET 4.7.2");
+            var versionInfo = GetVersionInfo();
+
+            Console.WriteLine($"assemblyVersion = {versionInfo.Item1} ");
+            Console.WriteLine($"fileVersion = {versionInfo.Item2} ");
+            Console.WriteLine($"productVersion = {versionInfo.Item3} ");
+
+            Console.ReadKey();
+        }
+
+        static Tuple<string, string, string> GetVersionInfo() {
+            string assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            
+            string fileVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion;
+            string productVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+
+            return new Tuple<string, string, string>(assemblyVersion, fileVersion, productVersion);
+        }
+    }
+}
